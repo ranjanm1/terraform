90474c62ef3e08778a36877111600fa56618813e
Ranjan Majumdar <ranjanm1@gmail.com>
2017-12-28 20:14:53 +0000

    Remove duplicated directory

--- udemy-dns-practice/london-web1-userdata.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-yum update -y
-yum install httpd24 -y
-service httpd start
-chkconfig httpd on
-echo "<html><body><h1>Hello, This is Prithiv (London Webserver 1)</h1></body></html>" > /var/www/html/index.html
